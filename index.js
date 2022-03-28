560956dd3afc6331f79e96b11c99f8467856d3b5
Joshua Toguri <joshua.toguri@gmail.com>
2022-03-28 14:58:26 -0400

    refactored function to confirm if user has completed exercise

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-const { exercises } = require("./data.js");
+const { exercises } = require('./data.js');
+const inquirer = require('inquirer');
 
 const getExercise = function() {
   const id = Math.floor(Math.random() * exercises.length);
   return exercises[id];
 }
 
-console.log(getExercise());
+const confirmCompletion = function() {
+  const questions = [{
+    type: 'confirm',
+    name: 'complete',
+    message: 'Did you complete this exercise?'
+  }];
+
+  return inquirer.prompt(questions).then(answers => {
+    return answers.complete;
+  });
+}
+
+let exercise = undefined;
+let today = [];
+
+const interaction = function() {
+  let complete = false;
+  let promiseState = 'new';
+
+  let intervalHandler = setInterval(() => {
+    if (promiseState === 'pending') {
+      return;
+    } else if (complete) {
+      today.push(exercise);
+      console.log(today);
+      return clearInterval(intervalHandler);
+    }
+
+    exercise = getExercise();
+    console.log(exercise);
+
+    promiseState = 'pending';
+
+    confirmCompletion().then(res => {
+      complete = res;
+      promiseState = 'complete';
+    });
+  }, 1);
+}
+
+interaction();
