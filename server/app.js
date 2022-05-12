22aa0af019e1577654c024c8bda619f8c932c5f6
Joshua Toguri <joshua.toguri@gmail.com>
2022-05-12 08:47:41 -0400

    refactoring the routes to improve readability

--- server/app.js
+++ server/app.js
@@ -3,18 +3,17 @@ const app = express();
 
 const port = 3001;
 
-const { dbconnect } = require('./db/index');
+const { conn } = require('./db/index');
 
 
-app.get('/api', (req, res) => {
-  dbconnect().then(db => {
-    return db.collection('exercises').find().toArray();
-  })
-  .then(exercises => res.send(exercises));
+app.get('/api', async (req, res) => {
+  const db = await conn();
+  const exercises = await db.collection('exercises').find().toArray();
+  res.send(exercises);
 });
 
 app.get('/api/randomExercise', (req, res) => {
-  dbconnect().then(db => {
+  conn().then(db => {
     return db.collection('exercises').aggregate([{$sample: {size: 1}}])
       .toArray();
   })
