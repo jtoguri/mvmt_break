1a0a343e0a4285a76edbed47b4381a713d710e03
Joshua Toguri <joshua.toguri@gmail.com>
2022-05-17 14:58:59 -0400

    update login form to be a react controlled component and add logic for axios request

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,38 @@
+import axios from 'axios';
+import { useState } from 'react';
 
 function Login () {
+  const [username, setUsername] = useState(""); 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.get('/api/login', {
+      username,
+      email,
+      password
+    });
+    console.log(res.data);
+  }
+
   return (
     <div>
       <h2>Sign In</h2>
       <p>Sign in to track your exercises.</p>
       
-      <form action="/api/login" method="get">
+      <form action="/api/login" method="get" onSubmit={handleSubmit}>
         <label htmlFor="username">Username: </label>
-        <input type="text" name="username" id="username" required />
+        <input type="text" name="username" id="username"
+          value={username} onChange={(e) => setUsername(e.target.value)} 
+            required />
         <label htmlFor="email">Email: </label>
-        <input type="email" name="email" id="email" required />
+        <input type="email" name="email" id="email" value={email} 
+          onChange={(e) => setEmail(e.target.value)} required />
         <label htmlFor="password">Password: </label>
-        <input type="password" name="password" id="password" required />
+        <input type="password" name="password" id="password"
+          vlaue={password} onChange={(e) => setPassword(e.target.value)} 
+            required />
         <input type="submit" value="Login" />
       </form>
     </div>
