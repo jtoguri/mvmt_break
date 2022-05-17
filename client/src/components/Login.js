658a02600acb2ad90f1c6e0e005886d6d3d406b4
Joshua Toguri <joshua.toguri@gmail.com>
2022-05-17 14:26:36 -0400

    add basic login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,7 +6,15 @@ function Login () {
       <h2>Sign In</h2>
       <p>Sign in to track your exercises.</p>
       
-      <p>Auth process here.</p>
+      <form action="/api/login" method="get">
+        <label htmlFor="username">Username: </label>
+        <input type="text" name="username" id="username" required />
+        <label htmlFor="email">Email: </label>
+        <input type="email" name="email" id="email" required />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" id="password" required />
+        <input type="submit" value="Login" />
+      </form>
     </div>
   )
 }
